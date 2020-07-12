e79335bd047178f1dcae57c5f69aba9c95c9eb0d
TWLaoluo <66513074+TWLaoluo@users.noreply.github.com>
2020-07-12 14:27:59 +0800

    F

--- goToElement.js
+++ goToElement.js
@@ -1,16 +1,28 @@
 window.onload = () => {
-    console.log('頁面再入成功!')
+    // console.log('頁面再入成功!')
 
-    var test = document.getElementById('test');
+    // 取得元素
+    // var test = document.getElementById('test');
+    var elements = document.getElementsByTagName('*');
 
-    test.onclick = () => {
-        var target = test.getAttribute('data-gte-target');
-        var offset = test.getAttribute('data-gte-offset');
+    for (let index = 0; index < elements.length; index++) {
+        const e = elements[index];
 
-        console.log(target);
-        console.log(offset);
+        e.onclick = () => {
+            var target = e.getAttribute('data-gte-target');  //目標屬性
+            var offset = e.getAttribute('data-gte-offset');  //位移元素
+            
+            if (target) {
+                var element = document.getElementById(target);      //目標元素
+    
+                // 視窗.捲動至({上方，行為})
+                window.scrollTo({
+                    top: element.offsetTop - offset,
+                    behavior:'smooth'
+                })
+            }
+        }
     }
-
 }
 
 
